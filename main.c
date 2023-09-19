f0ba7e8750aa98d0d79979c7620a5d784d955c1e
Alina Vorontsova <alina.vrts@inbox.ru>
2023-09-19 15:18:10 +0300

    new commit

--- main.c
+++ main.c
@@ -4,5 +4,5 @@ int main(int argc, char **argv) {
     char name[255];
 printf("Enter your name: ");
 fgets(name, 255, stdin);
-    printf("Hello %s!\n", name);
+    printf("Hello, %s!\n", name);
 return 0; }

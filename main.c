c060b46d977c81ed002ce557aae5f2722b0e4b83
Ilya Oleynikov <olieinikov_ilia@mail.ru>
2023-09-19 16:09:17 +0300

    vetv new

--- main.c
+++ main.c
@@ -5,3 +5,14 @@ int main(int argc, char **argv) {
     askname(first, last);
     printf("Hello ilia, %s %s!\n", first, last);
     return 0; }
+new vetka
+dfg
+dfg
+dfg
+df
+gdf
+g
+dflg
+dfkg
+df
+g
